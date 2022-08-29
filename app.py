8f3848f42377c3cedf84a4a4d16b4ec62ae7988f
waiee <waiee_z@yahoo.com>
2022-08-29 21:40:08 +0800

    hide trademark

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from PIL import Image
 
 st.title("House & Car Price Calculator (RM)")
 
+
+#Hide streamlit trademark
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+ 
