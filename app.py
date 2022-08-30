bd256fdb64a271ab6feb629ee29ce44b4fe8ee10
waiee <waiee_z@yahoo.com>
2022-08-30 22:50:00 +0800

    change st success

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ elif salary > 5000:
 else:
     dsr = dsr*salary*0.70
 
-st.success(f"Salary: RM {salary}")
-st.success(f"DSR (Debt Service Ratio): RM {dsr}")
+st.success(f"""Salary: RM {salary} 
+            \nDSR (Debt Service Ratio): RM {dsr}""")
 
 #find commitment
 
