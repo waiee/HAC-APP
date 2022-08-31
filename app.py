43fdf5d31f5802ed09134ab6a7aa045a0940c6e4
waiee <waiee_z@yahoo.com>
2022-08-31 22:26:09 +0800

    update app

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from PIL import Image
 
+#Hide streamlit trademark
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.title("House & Car Price Calculator")
 st.text("Created by Waiee Zainol")
 st.write("____")
@@ -31,45 +41,36 @@ housePrice = totalLoan*200
 #5. Loan limit (should be < 35% of salary)
 loanLimit = salary*0.35
 
+if salary and bankCom:
 #Spinner
-with st.spinner("Waiting .."):
-    time.sleep(5)
-st.success("Finished!")
+    import time
+    with st.spinner("Waiting .."):
+        time.sleep(3)
 
-st.success(f"""Your Salary: RM {salary} 
+    st.success(f"""Your Salary: RM {salary} 
             \nYour Bank Commitment: RM {bankCom}""")
 
-st.subheader("House")
-st.success(f"""Estimated Price: RM {housePrice}
+    st.subheader("House")
+    st.success(f"""Estimated Price: RM {housePrice}
             \nDSR (Debt Service Ratio): RM {dsr}
             \nMonthly Limit: RM {loanLimit}""")
 
 ##### CAR #####
 #1. Estimate car price
-carPrice = salary*12
+    carPrice = salary*12
 
 #2. Count downpayment
-downPayment = carPrice*0.20
+    downPayment = carPrice*0.20
 
 #3. Count loan period for 7 years in months
-loanPeriod = 12*7
+    loanPeriod = 12*7
 
 #4. Count monthly limit 
-monthlyLimit = salary*0.15
+    monthlyLimit = salary*0.15
 
-st.subheader("Car")
-st.success(f"""Estimated Price: RM {carPrice}
+    st.subheader("Car")
+    st.success(f"""Estimated Price: RM {carPrice}
             \nDownpayment (20%): RM {downPayment}
             \nLoan Duration: 7 years or {loanPeriod} months
             \nMonthly Limit: RM {monthlyLimit}""")
-
-#Hide streamlit trademark
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
  
