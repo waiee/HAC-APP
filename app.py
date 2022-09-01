f217c8249f645e251830b7e2fccb7887573c95cc
waiee <waiee_z@yahoo.com>
2022-09-01 22:50:31 +0800

    update app

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title("House & Car Price Calculator")
 st.text("Created by Waiee Zainol")
+st.text("Buat buat ada")
 st.write("____")
 salary = st.number_input("Enter your salary (RM)")
 
